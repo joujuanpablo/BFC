b3d4ccd9bbf475a9c739dfa644f4a313d32f0c9c
Juan Pablo Jou <jjouvalencia@deloitte.co.uk>
2018-12-27 14:29:45 -0500

    header change on scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,11 +10,24 @@ import cx from 'classnames'
 // Styles
 import headerStyles from './Header.module.scss'
 
-
 class Header extends React.Component {
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScrollHeader);
+      }
+      componentWillUnmount() {
+        window.addEventListener('scroll', this.handleScrollHeader);
+      }
+      handleScrollHeader() {
+        let scrollTop = window.scrollY
+        if (scrollTop > 2) {
+           document.querySelector("header").classList.add(headerStyles.scrolled);
+        } else {
+          document.querySelector("header").classList.remove(headerStyles.scrolled);
+        }
+    }
     render() {
         return (
-            <header className={headerStyles.header}>
+            <header className={cx(headerStyles.header)}>
                 <div className={cx('container', headerStyles.container)}>
                     { this.props.children }
                 </div>
