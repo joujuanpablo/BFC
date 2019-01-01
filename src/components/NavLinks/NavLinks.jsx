58db4caf740e63c68e7bf2512500baf8353503a1
Juan Pablo Jou <jjouvalencia@deloitte.co.uk>
2019-01-01 11:26:33 -0500

    fix syntactical error

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -4,7 +4,6 @@ import cx from 'classnames'
 
 // Components
 import { Link } from 'gatsby'
-import { Fragment } from 'react'
 
 // Utils
 import pageLinks from '../../utils/pageLinks.json';
@@ -12,16 +11,20 @@ import pageLinks from '../../utils/pageLinks.json';
 const links = pageLinks;
 
 export default (props) => {
-    <Fragment> {
-        links.map((page) => {
-            const { activeClass, linkClass, linkTextClass, scrolledClass, isScrolled } = props
-            return page.enabled &&
-                <Link to={page.path} title={`${page.title} page`} activeClassName={activeClass} className={linkClass} key={page.title}>
-                    <div className={cx(linkTextClass, isScrolled ? scrolledClass : null)}>
-                        {page.title}
-                    </div>
-                </Link>
-            })
-    }
-    </Fragment>
+    const { activeClass, linkClass, linkTextClass, scrolledClass, isScrolled } = props;
+
+    return(
+        <React.Fragment>
+            {
+            links.map((page) => {
+                return page.enabled &&
+                    <Link to={page.path} title={`${page.title} page`} activeClassName={activeClass} className={linkClass} key={page.title}>
+                        <div className={cx(linkTextClass, isScrolled ? scrolledClass : null)}>
+                            {page.title}
+                        </div>
+                    </Link>
+                })
+            }
+        </React.Fragment>
+    )
 }
