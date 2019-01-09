559842d9d43f523b46f87af5a97a67fa53267389
Juan Pablo Jou <jjouvalencia@deloitte.co.uk>
2019-01-09 16:06:33 -0500

    Footer finishing touches

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -88,6 +88,7 @@ export default () => (
                             )
                         })}
                     </div>
+                    <div className={footerStyles.credits}>Made by Cozy Cabin Consulting</div>
                 </div>
             </div>
         </div>
